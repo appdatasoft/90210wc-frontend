0c74653570ca61e83069d48b137a1b8ef044acf2
Saurabh Ambardekar <53165689+saurabhambardekar@users.noreply.github.com>
2020-07-27 23:24:26 +0530

    saurabh-task-app

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,11 @@ import IVDrip from "./app/screens/IVDrip";
 import Therapies from "./app/screens/Therapies";
 import Services from "./app/screens/Services";
 import Team from "./app/screens/Team";
+import EditServices from "./app/screens/EditServices";
+import EditIVDrips from "./app/screens/EditIVDrips";
+import EditTherapies from "./app/screens/EditTherapies";
+import EditTeam from "./app/screens/EditTeam";
+import AdminAdd from "./app/screens/Admin";
 
 function App() {
   return (
@@ -19,7 +24,13 @@ function App() {
           <Route exact path="/ivdrip/:slug" component={IVDrip} />
           <Route exact path="/therapies/:slug" component={Therapies} />
           <Route exact path="/services/:slug" component={Services} />
-          <Route exact path="/team/:slug" component={Team} />
+                  <Route exact path="/team/:slug" component={Team} />
+                  <Route exact path="/admin" component={AdminAdd} />
+
+                  <Route exact path="/admin/services" component={EditServices} />
+                  <Route exact path="/admin/ivdrips" component={EditIVDrips} />
+                  <Route exact path="/admin/therapies" component={EditTherapies} />
+                  <Route exact path="/admin/team" component={EditTeam} />
         </div>
       </div>
     </Router>
