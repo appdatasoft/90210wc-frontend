5b9c8de6fb036b2ccfd467b2e5f07fdedde0d0b4
VTVIVEK <vivek78643@gmail.com>
2020-07-28 21:53:06 +0530

    added graphql using @appollo/client

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,130 @@
-import React from "react";
-import "./App.css";
-import { HashRouter as Router, Route } from "react-router-dom";
+import React, { useRef, useEffect, useState } from "react";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  gql,
+} from "@apollo/client";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
 
+import "./App.css";
 import NavC from "./app/components/Nav";
 import Home from "./app/screens/Home";
-import IVDrip from "./app/screens/IVDrip";
+import Ivdrip from "./app/screens/IVDrip";
 import Therapies from "./app/screens/Therapies";
 import Services from "./app/screens/Services";
 import Team from "./app/screens/Team";
 
+const client = new ApolloClient({
+  uri: "https://vj0qrrbnk5.execute-api.us-east-1.amazonaws.com/dev/graphql",
+  cache: new InMemoryCache(),
+});
+
+const GET_MENU = gql`
+  query GetMenuData {
+    getIvdrips {
+      title
+      slug
+      description
+    }
+    getServices {
+      title
+      slug
+      description
+    }
+    getTeams {
+      title
+      slug
+      description
+    }
+    getTherapies {
+      title
+      slug
+      description
+    }
+  }
+`;
+
 function App() {
+  const [showLoader, setShowloader] = useState(true);
+  const [menuData, setMenuData] = useState({
+    ivdrips: [],
+    therapies: [],
+    services: [],
+    teams: [],
+  });
+
+  const getMenu = () => {
+    ref.current.continuousStart();
+    client
+      .query({
+        query: GET_MENU,
+      })
+      .then(({ data }) => {
+        setMenuData({
+          ivdrips: data.getIvdrips,
+          therapies: data.getTherapies,
+          teams: data.getTeams,
+          services: data.getServices,
+        });
+        ref.current.complete();
+        setShowloader(false);
+      })
+      .catch((err) => {
+        ref.current.complete();
+        setShowloader(false);
+      });
+  };
+  const ref = useRef(null);
+
+  useEffect(() => {
+    getMenu();
+  }, []);
+
   return (
-    <Router>
-      <div>
-        <NavC />
-        <div className="container">
-          <Route exact path="/" component={Home} />
-          <Route exact path="/ivdrip/:slug" component={IVDrip} />
-          <Route exact path="/therapies/:slug" component={Therapies} />
-          <Route exact path="/services/:slug" component={Services} />
-          <Route exact path="/team/:slug" component={Team} />
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          {showLoader ? (
+            <LoadingBar color="#f11946" ref={ref} />
+          ) : (
+            <>
+              <NavC data={menuData} />
+              <div className="container">
+                <Route exact path="/" component={Home} />
+                <Route
+                  exact
+                  path="/ivdrip/:slug"
+                  render={(props) => (
+                    <Ivdrip data={menuData.ivdrips} {...props} />
+                  )}
+                />
+                <Route
+                  exact
+                  path="/therapies/:slug"
+                  render={(props) => (
+                    <Therapies data={menuData.therapies} {...props} />
+                  )}
+                />
+                <Route
+                  exact
+                  path="/services/:slug"
+                  render={(props) => (
+                    <Services data={menuData.services} {...props} />
+                  )}
+                />
+                <Route
+                  exact
+                  path="/team/:slug"
+                  render={(props) => <Team data={menuData.teams} {...props} />}
+                />
+              </div>
+            </>
+          )}
         </div>
-      </div>
-    </Router>
+      </Router>
+    </ApolloProvider>
   );
 }
 
--- src/app/screens/IVDrip.jsx
+++ src/app/screens/IVDrip.jsx
@@ -1,34 +1,13 @@
 import React from "react";
-import axios from "axios";
 
-export default class IVDrip extends React.Component {
-  state = {
-    data: [],
-  };
-  componentDidMount() {
-    axios
-      .get("https://3usta2en23.execute-api.us-east-1.amazonaws.com/dev/ivdrips")
-      .then((res) => this.setState({ data: res.data.data.data }))
-      .catch((err) => console.log(err));
-  }
-  render() {
-    const result = this.state.data.find(
-      (d) => d.slug === this.props.match.params.slug
-    );
-    if (result) {
-      const { title, description } = result;
-      return (
-        <div>
-          <h1>{title}</h1>
-          <p>{description}</p>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <h1>No result found</h1>
-        </div>
-      );
-    }
-  }
-}
+const Ivdrip = ({ data, match }) => {
+  const { title, description } = data.find((d) => d.slug === match.params.slug);
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{description}</p>
+    </div>
+  );
+};
+
+export default Ivdrip;
